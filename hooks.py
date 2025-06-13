4a563722a73dfad770bfbf33c28612329d728563
mr-mapache <eric.m.cardozo@mi.unc.edu.ar>
2025-06-13 21:32:19 +0000

    Update mkdocs

---
+++ hooks.py
@@ -0,0 +1,18 @@
+import shutil
+import os
+
+def copy_folder(*args, **kwargs):
+    src = os.path.abspath("stylesheets")
+    dest = os.path.abspath("contemporary-physics/stylesheets")
+     
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    shutil.copytree(src, dest)
+
+def cleanup_folder(*args, **kwargs):
+    dest = os.path.abspath("contemporary-physics/stylesheets")
+    
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+        print(f"Cleaned up: {dest}")
+
